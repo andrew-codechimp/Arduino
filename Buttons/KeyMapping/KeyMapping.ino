038db0c7c9c16ace6eccb7cdc24e6660930a6397
andrew <andrew@codechimp.org>
2014-12-20 09:15:18 +0000

    Fixed flutter and changed keymappings for android studio

--- Buttons/KeyMapping/KeyMapping.ino
+++ Buttons/KeyMapping/KeyMapping.ino
@@ -1,5 +1,11 @@
 #include "Arduino.h"
 
+// Ubuntu
+// to upload there must be a-star.rules in /etc/udev/rules.d
+// to run remove/rename file
+// reset the board after each change
+
+
 // constants won't change. They're used here to 
 // set pin numbers:
 const int buttonPinRed = 1;    // the number of the red pushbutton pin
@@ -15,14 +21,14 @@ boolean lastStateBlack = LOW;//storage for last measured button state
 boolean debouncedStateBlack = LOW;//debounced button state
 
 
-int debounceInterval = 100;//wait in Ams for button pin to settle
+int debounceInterval = 20;//wait in Ams for button pin to settle
 unsigned long timeOfLastButtonEventRed = 0;//store the last time the button state changed
 unsigned long timeOfLastButtonEventBlack = 0;//store the last time the button state changed
 
 void setup(){
   Serial.begin(9600);
-  pinMode(buttonPinRed, INPUT);
-  pinMode(buttonPinBlack, INPUT);
+  pinMode(buttonPinRed, INPUT_PULLUP);
+  pinMode(buttonPinBlack, INPUT_PULLUP);
   Keyboard.begin();
 }
 
@@ -43,8 +49,8 @@ void loop() {
       //trigger an event
       if (debouncedStateRed == HIGH){
         Keyboard.press(KEY_LEFT_SHIFT);
-        //Keyboard.press(KEY_F10);
-        Keyboard.press('R');
+        Keyboard.press(KEY_F10);
+        //Keyboard.press('R');
         Keyboard.releaseAll();
         delay(50);
       }
@@ -66,8 +72,8 @@ void loop() {
       //trigger an event
       if (debouncedStateBlack == HIGH){
         Keyboard.press(KEY_LEFT_SHIFT);
-        //Keyboard.press(KEY_F9);
-        Keyboard.press('B');
+        Keyboard.press(KEY_F9);
+        //Keyboard.press('B');
         Keyboard.releaseAll();
         delay(50);
       }
@@ -76,6 +82,7 @@ void loop() {
 
   lastStateBlack = currentStateBlack;
 }
+
 
 
 
