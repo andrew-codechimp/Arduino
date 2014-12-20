d42a1b4b124781bcb8efcf80b6138fb3be03a9a8
andrew <andrew@codechimp.org>
2014-12-20 08:58:30 +0000

    Add keyboard shortcuts

---
+++ Buttons/KeyMapping/KeyMapping.ino
@@ -0,0 +1,83 @@
+#include "Arduino.h"
+
+// constants won't change. They're used here to 
+// set pin numbers:
+const int buttonPinRed = 1;    // the number of the red pushbutton pin
+const int buttonPinBlack = 0;    // the number of the black pushbutton pin
+
+
+boolean currentStateRed = LOW;//stroage for current measured button state
+boolean lastStateRed = LOW;//storage for last measured button state
+boolean debouncedStateRed = LOW;//debounced button state
+
+boolean currentStateBlack = LOW;//stroage for current measured button state
+boolean lastStateBlack = LOW;//storage for last measured button state
+boolean debouncedStateBlack = LOW;//debounced button state
+
+
+int debounceInterval = 100;//wait in Ams for button pin to settle
+unsigned long timeOfLastButtonEventRed = 0;//store the last time the button state changed
+unsigned long timeOfLastButtonEventBlack = 0;//store the last time the button state changed
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(buttonPinRed, INPUT);
+  pinMode(buttonPinBlack, INPUT);
+  Keyboard.begin();
+}
+
+void loop() {
+  unsigned long currentTime = millis();    
+
+  //Red  
+  currentStateRed = digitalRead(buttonPinRed);
+
+  if (currentStateRed != lastStateRed){
+    timeOfLastButtonEventRed = currentTime;
+  }
+
+  if (currentTime - timeOfLastButtonEventRed > debounceInterval){//if enough time has passed
+    if (currentStateRed != debouncedStateRed){//if the current state is still different than our last stored debounced state
+      debouncedStateRed = currentStateRed;//update the debounced state
+
+      //trigger an event
+      if (debouncedStateRed == HIGH){
+        Keyboard.press(KEY_LEFT_SHIFT);
+        //Keyboard.press(KEY_F10);
+        Keyboard.press('R');
+        Keyboard.releaseAll();
+        delay(50);
+      }
+    }
+  }
+  lastStateRed = currentStateRed;
+
+  //Black
+  currentStateBlack = digitalRead(buttonPinBlack);
+
+  if (currentStateBlack != lastStateBlack){
+    timeOfLastButtonEventBlack = currentTime;
+  }
+
+  if (currentTime - timeOfLastButtonEventBlack > debounceInterval){//if enough time has passed
+    if (currentStateBlack != debouncedStateBlack){//if the current state is still different than our last stored debounced state
+      debouncedStateBlack = currentStateBlack;//update the debounced state
+
+      //trigger an event
+      if (debouncedStateBlack == HIGH){
+        Keyboard.press(KEY_LEFT_SHIFT);
+        //Keyboard.press(KEY_F9);
+        Keyboard.press('B');
+        Keyboard.releaseAll();
+        delay(50);
+      }
+    }
+  }
+
+  lastStateBlack = currentStateBlack;
+}
+
+
+
+
+
